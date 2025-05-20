de66fc42bab2790aa03c382dc8b8caffae013e45
RitaRublevskaya06 <144614540+RitaRublevskaya06@users.noreply.github.com>
2025-05-20 22:50:48 +0300

    Add files via upload

---
+++ 4sem/OOP/Lab_01/Lab_01/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01
+{
+    class Calculator
+    {
+        private static int Evaluate(int first, int second, string operation)
+        {
+            switch (operation[0])
+            {
+                case '&':
+                    return first & second;
+                case '|':
+                    return first | second;
+                case '^':
+                    return first ^ second;
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        public static int Parse(string[] source)
+        {
+            if (source.Length < 2)
+                throw new IndexOutOfRangeException();
+            if (source[source.Length - 1][0] == '~')
+            {
+                if (Int32.TryParse(source[source.Length - 2], out int negInt))
+                    return ~negInt;
+                else
+                    throw new InvalidOperationException();
+            }
+            if (source.Length < 3)
+                throw new IndexOutOfRangeException();
+            if (Int32.TryParse(source[source.Length - 1 - 2], out int firstValue)
+                && Int32.TryParse(source[source.Length - 1], out int secondValue))
+                return Evaluate(firstValue, secondValue, source[source.Length - 1 - 1]);
+            else
+                throw new InvalidOperationException();
+        }
+    }
+}
